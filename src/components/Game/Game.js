a31ef54aab5370f974566ecf2aafaa2df72f05d5
haikalbintang <mhaikalbintang.work@gmail.com>
2024-06-05 00:43:58 +0700

    Add exercise 1.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,31 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ guess });
+    setGuess("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        value={guess}
+        onChange={(e) => {
+          const nextGuess = e.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+        required
+        minLength={5}
+        maxLength={5}
+      />
+    </form>
+  );
 }
 
 export default Game;
